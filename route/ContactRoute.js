b98078366003d262f4ebbbdc81a299239b09e8df
unknown <kumarritik588@gmail.com>
2022-12-10 11:17:51 +0530

    change

--- route/ContactRoute.js
+++ route/ContactRoute.js
@@ -5,7 +5,7 @@ const { getAll, add } = require("../Controller/ContactController");
 const router = require("express").Router();
 // const { add, getAll } = require("../controllers/ApplicationCont");
 router.get("/getAll", getAll);
-router.post("/add", add);
+router.post("/add", add); 
 router.get("/text", (req, res) => {
     res.send("Server is running");
   });
--- route/JobsFormRoute.js
+++ route/JobsFormRoute.js
@@ -8,3 +8,4 @@ router.get("/getAll", getAll);
 router.post("/add", add);
 
 module.exports = router;
+
